7c01ef99add09f17212a4cb4e9809c1de13b2210
Ricki Peikes <rickipeikes@gmail.com>
2020-12-10 23:01:41 -0500

    added draw centered string method

--- src/main/java/FC/CardView.java
+++ src/main/java/FC/CardView.java
@@ -6,10 +6,14 @@ import java.awt.*;
 
 public class CardView extends JComponent {
     public static final Color CARD_COLOR = Color.WHITE;
-    //public static final Color TEXT_COLOR = Color.BLACK;
+    public static final Color TEXT_COLOR = Color.BLACK;
+    public static final int CARD_WIDTH = 500;
+    public static final int CARD_HEIGHT = 300;
+    public static final Rectangle rect = new Rectangle(CARD_WIDTH, CARD_HEIGHT);
+    public static final Font font = new Font("Arial", Font.PLAIN, 20);
     private String cardText;
 
-    public void setCardText(String cardText){
+    public void setCardText(String cardText) {
         this.cardText = cardText;
     }
 
@@ -21,21 +25,21 @@ public class CardView extends JComponent {
 
     private void paintCard(Graphics g) {
         g.setColor(CARD_COLOR);
-        g.fillRect(0,0,250,150);
-        g.drawString(String.format("<html><body style=\"text-align: justify;  " +
-                "text-justify: inter-word;\">%s</body></html>",cardText),5,5);
+        g.fillRect(0, 0, CARD_WIDTH, CARD_HEIGHT);
+        drawCenteredString(g);
     }
 
-//    private void drawCenteredString(Graphics g, String text, Rectangle rect, Font font) {
-//        // Get the FontMetrics
-//        FontMetrics metrics = g.getFontMetrics(font);
-//        // Determine the X coordinate for the text
-//        int x = rect.x + (rect.width - metrics.stringWidth(text)) / 2;
-//        // Determine the Y coordinate for the text (note we add the ascent, as in java 2d 0 is top of the screen)
-//        int y = rect.y + ((rect.height - metrics.getHeight()) / 2) + metrics.getAscent();
-//        // Set the font
-//        g.setFont(font);
-//        // Draw the String
-//        g.drawString(text, x, y);
-//    }
+    private void drawCenteredString(Graphics g) {
+        g.setColor(TEXT_COLOR);
+        // Get the FontMetrics
+        FontMetrics metrics = g.getFontMetrics(font);
+        // Determine the X coordinate for the text
+        int x = rect.x + (rect.width - metrics.stringWidth(cardText)) / 2;
+        // Determine the Y coordinate for the text (note we add the ascent, as in java 2d 0 is top of the screen)
+        int y = rect.y + ((rect.height - metrics.getHeight()) / 2) + metrics.getAscent();
+        // Set the font
+        g.setFont(font);
+        // Draw the String
+        g.drawString(cardText, x, y);
+    }
 }
